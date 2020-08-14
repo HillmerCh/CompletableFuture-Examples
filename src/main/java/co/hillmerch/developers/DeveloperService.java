7fd1ec6d4fa38aa57e3dc82aa51e3b75d503d281
HillmerCh <hilac@msn.com>
2020-08-13 19:53:10 -0500

    Adding third example
    Signed-off-by: HillmerCh <hilac@msn.com>

--- src/main/java/co/hillmerch/developers/DeveloperService.java
+++ src/main/java/co/hillmerch/developers/DeveloperService.java
@@ -4,68 +4,101 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 public class DeveloperService {
 
-	public List<Developer> generateDeveloperCurrentStatus(){
+	public List<Developer> generateDeveloperCurrentStatus() {
 		List<Developer> developerList = this.findDevelopers();
-		for(Developer developer:developerList){
+		for ( Developer developer : developerList ) {
 			developer = this.loadNumberOfTasks( developer );
 		}
 		return developerList;
 	}
 
-	public List<Developer> generateDeveloperCurrentStatusAsync(){
+	public List<Developer> generateDeveloperCurrentStatusAsync() {
 		List<Developer> developerList = this.findDevelopers();
-		List<CompletableFuture<Developer>>  cfList = new ArrayList<>();
-		for(Developer developer:developerList){
-			CompletableFuture cf = CompletableFuture.supplyAsync( () -> this.loadNumberOfTasks( developer ));
+		List<CompletableFuture<Developer>> cfList = new ArrayList<>();
+		for ( Developer developer : developerList ) {
+			CompletableFuture cf = CompletableFuture.supplyAsync( () -> this.loadNumberOfTasks( developer ) );
 			cfList.add( cf );
 		}
-		CompletableFuture.allOf( cfList.toArray(new CompletableFuture[0])).join();
+		CompletableFuture.allOf( cfList.toArray( new CompletableFuture[0] ) ).join();
 		developerList = cfList.stream()
-				.filter( CompletableFuture::isDone  )
+				.filter( CompletableFuture::isDone )
 				.map( CompletableFuture::join )
 				.collect( Collectors.toList() );
 		return developerList;
 	}
 
-	private List<Developer> findDevelopers(){
+	private List<Developer> findDevelopers() {
 		// Este método simula la consulta de los desarrolladores generalmente sobre una base de datos
 		return List
-				.of(Developer.of( 1L, "Developer 1" ),
-					Developer.of( 2L, "Developer 2" ),
-					Developer.of( 3L, "Developer 3" ),
-					Developer.of( 4L, "Developer 4" ),
-					Developer.of( 5L, "Developer 5" ),
-					Developer.of( 6L, "Developer 6" ),
-					Developer.of( 7L, "Developer 7" ),
-					Developer.of( 8L, "Developer 8" ),
-					Developer.of( 9L, "Developer 9" ),
-					Developer.of( 10L, "Developer 10" )
-					);
+				.of(
+						Developer.of( 1L, "Developer 1" ),
+						Developer.of( 2L, "Developer 2" ),
+						Developer.of( 3L, "Developer 3" ),
+						Developer.of( 4L, "Developer 4" ),
+						Developer.of( 5L, "Developer 5" ),
+						Developer.of( 6L, "Developer 6" ),
+						Developer.of( 7L, "Developer 7" ),
+						Developer.of( 8L, "Developer 8" ),
+						Developer.of( 9L, "Developer 9" ),
+						Developer.of( 10L, "Developer 10" )
+				);
 	}
 
-	private Developer loadNumberOfTasks(Developer developer){
+	private Developer loadNumberOfTasks(Developer developer) {
 		// Este método simula la consulta del número de tareas asignadas a un desarrollador
 		// Se genera un valor aleatorio para simular el número de tareas pendientes, en progreso y finalizadas
 		// por cada desarrollador
 		Random random = new Random();
-		developer.setTaskToDo( random.ints( 0, 20).findAny().getAsInt() );
-		developer.setTaskDone( random.ints( 0, 20).findAny().getAsInt() );
-		developer.setTaskInProgress( random.ints( 0, 20).findAny().getAsInt() );
+		developer.setTaskToDo( random.ints( 0, 20 ).findAny().getAsInt() );
+		developer.setTaskDone( random.ints( 0, 20 ).findAny().getAsInt() );
+		developer.setTaskInProgress( random.ints( 0, 20 ).findAny().getAsInt() );
 
-		this.sleep( 2_000);//Una pausa de 1 segundo para simular el tiempo que demora la carga de las tareas
+		this.sleep( 2_000 );//Una pausa de 1 segundo para simular el tiempo que demora la carga de las tareas
 		return developer;
 	}
 
 	private void sleep(long time) {
 		try {
 			Thread.sleep( time );
-		}catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
+
+	private void printPoolInfo(){
+		System.out.println( "Pool size: " + ForkJoinPool.commonPool().getPoolSize()
+				+ ". Thread:  " + Thread.currentThread().getName());
+	}
+
+	public List<Developer> generateDeveloperCurrentStatusAsyncWithExecutor(Executor executor) {
+		List<Developer> developerList = this.findDevelopers();
+		List<CompletableFuture<Developer>> cfList = new ArrayList<>();
+
+		printPoolInfo();
+
+		for ( Developer developer : developerList ) {
+			CompletableFuture cf = CompletableFuture.supplyAsync( () -> this.loadNumberOfTasks( developer ), executor)
+					.thenRun( () -> printPoolInfo());
+			cfList.add( cf );
+		}
+
+		printPoolInfo();
+		CompletableFuture.allOf( cfList.toArray( new CompletableFuture[0] ) ).join();
+		developerList = cfList.stream()
+				.filter( CompletableFuture::isDone )
+				.map( CompletableFuture::join )
+				.collect( Collectors.toList() );
+
+		printPoolInfo();
+		return developerList;
+	}
+
 
 }
--- src/test/java/co/hillmerch/developers/DeveloperServiceTest.java
+++ src/test/java/co/hillmerch/developers/DeveloperServiceTest.java
@@ -3,6 +3,9 @@ package co.hillmerch.developers;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 
 import org.junit.jupiter.api.Test;
 
@@ -33,6 +36,28 @@ class DeveloperServiceTest {
 		logger.info( developerList.toString() );
 
 		logger.info( "Tiempo de respuesta: " + Duration.between( start, Instant.now()).toString());
+	}
+
+	@Test
+	void testingGenerateDeveloperCurrentStatusAsyncWithExecutors() {
+		Instant start = null;//Variable usada para contar el tiempo en que se tiene respuesta del método
+
+		DeveloperService developerService = new DeveloperService();
+
+		logger.info( "Prueba con Executor: Single Thread");
+		start = Instant.now();
+		List<Developer> developerList = developerService.generateDeveloperCurrentStatusAsyncWithExecutor( Executors.newSingleThreadExecutor());
+		logger.info( "Tiempo de respuesta (Single Thread): " + Duration.between( start, Instant.now()).toString());
+
+		logger.info( "Prueba con Executor: Common Pool");
+		start = Instant.now();
+		List<Developer> developerList2 = developerService.generateDeveloperCurrentStatusAsyncWithExecutor(ForkJoinPool.commonPool());
+		logger.info( "Tiempo de respuesta (Common Pool): " + Duration.between( start, Instant.now()).toString());
+
+		logger.info( "Prueba con Executor: Fixed Thread");
+		start = Instant.now();
+		List<Developer> developerList3 = developerService.generateDeveloperCurrentStatusAsyncWithExecutor(Executors.newFixedThreadPool(50));
+		logger.info( "Tiempo de respuesta (Fixed Thread): " + Duration.between( start, Instant.now()).toString());
 	}
 
 }
